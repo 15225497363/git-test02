a5fd554c50024976be906051745db59803dde069
wyb <1793031607@qq.com>
2018-10-19 22:19:49 +0800

    这是dev分支提交

--- src/main/java/com/baizhi/tsts/TstsJ.java
+++ src/main/java/com/baizhi/tsts/TstsJ.java
@@ -7,5 +7,6 @@ public class TstsJ {
     public static void main(String[] args) {
         System.out.println("haha");
         System.out.println("xixi");
+        System.out.println("我是dev分支");
     }
 }
